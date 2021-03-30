e347b40fccd669833ad2d4ca009c659bb69c4b3e
GP-R <48301097+GP-R@users.noreply.github.com>
2021-03-30 11:46:42 +0900

    quiz #2

--- OOP_quiz/OOP_quiz/quiz.cpp
+++ OOP_quiz/OOP_quiz/quiz.cpp
@@ -16,23 +16,42 @@ void excludeRedundantLetters(char* str)
 {
 	char space[100] = "";
 	int count = 0;
+	int count2 = 0;
 	for (int i = 0; str[i] != '\0'; i++)
 	{
 		for (int j = 0; j < strlen(space); j++)
 		{
-			if (space[j] != str[i])
+			if (space[j] == str[i])
 			{
-				space[count] = str[i];
-				count++;
+				count2++;
 			}
 		}
+		if (count2 == 0)
+		{
+			space[count] = str[i];
+			count++;
+		}
+		count2 = 0;
 	}
 	for (int i = 0; i < strlen(space); i++)
 	{
 		str[i] = space[i];
 	}
-	str[strlen(space) + 1] = '\0';
+	str[strlen(space)] = '\0';
 }
+
+/*int countWords(const char* str)
+{
+	int count = 0;
+	for (; *str != '\0'; str++)
+	{
+		if (*str != 32 && *str != 10)
+			str++;
+		if(*)
+			count++;
+	}
+	return count;
+}*/
 
 void main()
 {
