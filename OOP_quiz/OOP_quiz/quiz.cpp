0a620223b2d77961d420e971748f8859c68fc389
GP-R <48301097+GP-R@users.noreply.github.com>
2021-03-30 10:45:21 +0900

    quiz #2

--- OOP_quiz/OOP_quiz/quiz.cpp
+++ OOP_quiz/OOP_quiz/quiz.cpp
@@ -11,14 +11,36 @@ int countMatchedNumberLetterInString(const char *str, int digit)
 	}
 	return count;
 }
+// quiz 2
+void excludeRedundantLetters(char* str)
+{
+	char space[100] = "";
+	int count = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		for (int j = 0; j < strlen(space); j++)
+		{
+			if (space[j] != str[i])
+			{
+				space[count] = str[i];
+				count++;
+			}
+		}
+	}
+	for (int i = 0; i < strlen(space); i++)
+	{
+		str[i] = space[i];
+	}
+	str[strlen(space) + 1] = '\0';
+}
 
 void main()
 {
-	int count = countMatchedNumberLetterInString("11122233333444555", 3);
-	printf("%d", count);
-	//char data[100]{ "Hello World" };
-	//excludeRedundantLetters(data);
-	//printf("%s", data);
+	//int count = countMatchedNumberLetterInString("11122233333444555", 3);
+	//printf("%d", count);
+	char data[100]{ "Hello World" };
+	excludeRedundantLetters(data);
+	printf("%s", data);
 	//int count2 = countWords("Hello World, Here is train\n");
 	//printf("%d", count2);
 
