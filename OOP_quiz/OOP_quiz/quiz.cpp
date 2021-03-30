3ce83660cc2158b57c0611d90f17e8032b406708
GP-R <48301097+GP-R@users.noreply.github.com>
2021-03-31 00:02:18 +0900

    quiz #5

--- OOP_quiz/OOP_quiz/quiz.cpp
+++ OOP_quiz/OOP_quiz/quiz.cpp
@@ -1,5 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<cctype>
+#include<string.h>
 //quiz 1
 int countMatchedNumberLetterInString(const char *str, int digit)
 {
@@ -16,7 +18,8 @@ int countMatchedNumberLetterInString(const char *str, int digit)
 void excludeRedundantLetters(char* str)
 {
 	char space[100] = "";
-	int count = 0;
+	//char* space = new char;
+	int index = 0;
 	int count2 = 0;
 	for (int i = 0; str[i] != '\0'; i++)
 	{
@@ -29,8 +32,8 @@ void excludeRedundantLetters(char* str)
 		}
 		if (count2 == 0)
 		{
-			space[count] = str[i];
-			count++;
+			space[index] = str[i];
+			index++;
 		}
 		count2 = 0;
 	}
@@ -39,6 +42,7 @@ void excludeRedundantLetters(char* str)
 		str[i] = space[i];
 	}
 	str[strlen(space)] = '\0';
+	//delete space;
 }
 //quiz 3
 int countWords(const char* str)
@@ -66,17 +70,43 @@ bool examinePairedParenthesis(const char* str)
 		return true;
 	return false;
 }
-
+//quiz 5
+void convertString(char* input)
+{
+	char str[100] = "";
+	int count = 0;
+	for (int i = 0; i < strlen(input); i++)
+	{
+		if (input[i] == 32)
+		{
+			str[i +count] = '%';
+			str[i +count + 1] = '2';
+			str[i +count + 2] = '0';
+			count += 2;
+		}
+		else
+			str[i + count] = input[i]; //input[0] =h ... e l l o input[5] = '' str[5 ,6 ,7] %20 str[8] = str[6]
+	}
+	strcpy(input, str);
+	return;
+}
 void main()
 {
 	//int count = countMatchedNumberLetterInString("11122233333444555", 3);
 	//printf("%d", count);
+
 	//char data[100]{ "Hello World" };
 	//excludeRedundantLetters(data);
 	//printf("%s", data);
+
 	//int count2 = countWords("Hello World, Here is train\n");
 	//printf("%d", count2);
-	bool result = examinePairedParenthesis("((()))"); // 40 41
-	printf("%s", result == true ? "true" : "false");
+
+	//bool result = examinePairedParenthesis("((()))"); // 40 41
+	//printf("%s", result == true ? "true" : "false");
+
+	char input[100] = "Hello World.Nice to Meet you.";
+	convertString(input);
+	printf("%s\n", input);
 	return;
 }
